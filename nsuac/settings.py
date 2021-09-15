6698fedfd81e061b47cd96db485699f62d059ae2
HridoyAlam <jahidul.alam@northsouth.edu>
2021-09-16 00:41:18 +0600

    update

--- nsuac/settings.py
+++ nsuac/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-vb!%iljj#a51rzaepf%821%4vlob3d-5ql@o3y!kje0$m1ri^(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nsuac.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
